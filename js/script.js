f3c8e9b3a3514a9b515a2779d0a9ee621ba6e162
Guz117 <cristian.guzman117@gmail.com>
2021-12-02 14:59:16 +0100

    tentativo 1 per colorare i quadrati

--- js/script.js
+++ js/script.js
@@ -7,6 +7,36 @@ const colNormal = 9;
 const rowCrazy = 7;
 const colCrazy = 7;
 
+// creo tre array di 16 numeri casuali per ogni griglia
+
+let bombNumber100 = [];
+console.log(bombNumber100);
+let bombNumber81 = [];
+console.log(bombNumber81);
+let bombNumber49 = [];
+console.log(bombNumber49);
+  
+while (bombNumber100.length < 16) {
+  const randomNumber100  = Math.floor(Math.random() * 100) + 1; 
+  if (!bombNumber100.includes(randomNumber100)) {
+      bombNumber100.push(randomNumber100);
+  }
+}
+
+while (bombNumber81.length < 16) {
+    const randomNumber81  = Math.floor(Math.random() * 81) + 1; 
+    if (!bombNumber100.includes(randomNumber81)) {
+      bombNumber81.push(randomNumber81);
+  }
+}
+
+while (bombNumber49.length < 16) {
+    const randomNumber49  = Math.floor(Math.random() * 49) + 1; 
+    if (!bombNumber100.includes(randomNumber49)) {
+      bombNumber49.push(randomNumber49);
+  }
+}
+
 const container = document.querySelector('.container');
 const button = document.getElementById('play');
 
@@ -22,8 +52,13 @@ button.addEventListener('click',
             const square = document.createElement('div');
             square.classList.add('square');
             square.addEventListener('click', function () {
-                this.style.background = '#6495ed';
-                this.style.color = 'white'
+                if (i == 32) {
+                    this.style.background = 'red';
+                    this.style.color = 'white'
+                } else {
+                    this.style.background = '#6495ed';
+                    this.style.color = 'white'
+                }
             } );
             square.style.width = `calc(100% / ${colEasy})`;
             square.style.height = `calc(100% / ${rowEasy})`;
@@ -35,8 +70,13 @@ button.addEventListener('click',
             const square = document.createElement('div');
             square.classList.add('square');
             square.addEventListener('click', function () {
-                this.style.background = '#6495ed';
-                this.style.color = 'white'
+                if (i == 32) {
+                    this.style.background = 'red';
+                    this.style.color = 'white'
+                } else {
+                    this.style.background = '#6495ed';
+                    this.style.color = 'white'
+                }
             } );
             square.style.width = `calc(100% / ${colNormal})`;
             square.style.height = `calc(100% / ${rowNormal})`;
@@ -48,8 +88,13 @@ button.addEventListener('click',
             const square = document.createElement('div');
             square.classList.add('square');
             square.addEventListener('click', function () {
-                this.style.background = '#6495ed';
-                this.style.color = 'white'
+                if (i == 32) {
+                    this.style.background = 'red';
+                    this.style.color = 'white'
+                } else {
+                    this.style.background = '#6495ed';
+                    this.style.color = 'white'
+                }
             } );
             square.style.width = `calc(100% / ${colCrazy})`;
             square.style.height = `calc(100% / ${rowCrazy})`;
@@ -59,32 +104,4 @@ button.addEventListener('click',
     }
   });
 
-// creo tre array di 16 numeri casuali per ogni griglia
 
-  let bombNumber100 = [];
-  console.log(bombNumber100);
-  let bombNumber81 = [];
-  console.log(bombNumber81);
-  let bombNumber49 = [];
-  console.log(bombNumber49);
-    
-  while (bombNumber100.length < 16) {
-    const randomNumber100  = Math.floor(Math.random() * 100) + 1; 
-    if (!bombNumber100.includes(randomNumber100)) {
-        bombNumber100.push(randomNumber100);
-    }
-  }
-
-  while (bombNumber81.length < 16) {
-      const randomNumber81  = Math.floor(Math.random() * 81) + 1; 
-      if (!bombNumber100.includes(randomNumber81)) {
-        bombNumber81.push(randomNumber81);
-    }
-  }
-  
-  while (bombNumber49.length < 16) {
-      const randomNumber49  = Math.floor(Math.random() * 49) + 1; 
-      if (!bombNumber100.includes(randomNumber49)) {
-        bombNumber49.push(randomNumber49);
-    }
-  }
